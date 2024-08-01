85be3090004fdbb7c59303102390e9cd8e25ffac
Mehadees <mehadees004@gmail.com>
2024-08-01 23:24:15 +0530

    resume added

--- pro.js
+++ pro.js
@@ -1,8 +1,12 @@
 var cbox = document.querySelector(".cbox");
 var clbtn = document.querySelector(".clbtn");
+var bg = document.querySelector(".bg");
 function show(){
     cbox.style.display="block";
+    bg.style.display="block"
 }
 clbtn.addEventListener("click",function(){
     cbox.style.display="none";
+    bg.style.display="none";
 }) 
+    
